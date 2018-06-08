17d3b4bedebe524e5c3d6be997b5dae5c6a75122
Jon Andersson <andersson.jon@gmail.com>
2018-06-08 10:41:03 +0200

    And we have an android build.

--- android/src/main/java/com/reactlibrary/HDSDSReactNativeChartbeatSdkModule.java
+++ android/src/main/java/com/reactlibrary/HDSDSReactNativeChartbeatSdkModule.java
@@ -1,10 +1,13 @@
 
 package com.reactlibrary;
 
+import android.app.Application;
+import android.util.Log;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.chartbeat.androidsdk.Tracker;
 
 public class HDSDSReactNativeChartbeatSdkModule extends ReactContextBaseJavaModule {
 
@@ -19,4 +22,12 @@ public class HDSDSReactNativeChartbeatSdkModule extends ReactContextBaseJavaModu
   public String getName() {
     return "HDSDSReactNativeChartbeatSdk";
   }
+
+  @ReactMethod
+  public void setupTracker(int withAccountId, String domain) {
+    Application applicationContext = (Application) reactContext.getApplicationContext();
+    Log.w("myApp", "no network");
+    Tracker.setupTracker("54876", "androidsdktest.chartbeat.com", applicationContext);
+  }
+  public void trackView()
 }
