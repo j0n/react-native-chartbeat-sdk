52c3a5d333ef37f13ed8dbaff7bd48133779067c
Jon Andersson <andersson.jon@gmail.com>
2018-06-12 10:09:27 +0200

    `setupTracker` works differently on iOS and android.

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
-import { NativeModules } from 'react-native';
+import { NativeModules, Platform } from 'react-native';
 
 const { HDSDSReactNativeChartbeatSdk } = NativeModules;
 
-export default HDSDSReactNativeChartbeatSdk ;
+const setupTracker = function (accountId, domain) {
+  if (Platform.OS === 'android') {
+    // accountId need to be an string
+    if (typeof accountId === 'string') {
+      HDSDSReactNativeChartbeatSdk.setupTracker(accountId, domain);
+    }
+  } else {
+    // accountId need to be an number
+    if (typeof accountId === 'number') {
+      HDSDSReactNativeChartbeatSdk.setupTracker(accountId, domain);
+    } else {
+      try {
+        const parsedNumber = Number.parseInd(accountId)
+        if (Number.isNaN(parsedNumber)) {
+          throw new Error('invalid account id');
+        }
+        HDSDSReactNativeChartbeatSdk.setupTracker(parsedNumber, domain);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
+}
+
+export default {
+  setupTracker,
+  trackView: HDSDSReactNativeChartbeatSdk.trackView,
+  setAuthors: HDSDSReactNativeChartbeatSdk.setAuthors,
+  setSections: HDSDSReactNativeChartbeatSdk.setSections,
+}
