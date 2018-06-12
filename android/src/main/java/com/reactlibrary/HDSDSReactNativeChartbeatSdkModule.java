9133c315ffd6f68e9e7a30c236e3d4921b01ed9a
Jon Andersson <andersson.jon@gmail.com>
2018-06-12 10:08:48 +0200

    set DEBUG to true for android, everything seems fine, but nothing i logged in the dashboard. Contacted the support for chartbeat.

--- android/src/main/java/com/reactlibrary/HDSDSReactNativeChartbeatSdkModule.java
+++ android/src/main/java/com/reactlibrary/HDSDSReactNativeChartbeatSdkModule.java
@@ -3,13 +3,19 @@ package com.reactlibrary;
 
 import android.app.Activity;
 import android.app.Application;
-import android.util.Log;
+
+import java.util.*;
 import java.util.Collection;
+import android.util.Log;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.ReadableArray;
 import com.chartbeat.androidsdk.Tracker;
+
+
+
 
 public class HDSDSReactNativeChartbeatSdkModule extends ReactContextBaseJavaModule {
 
@@ -26,20 +32,36 @@ public class HDSDSReactNativeChartbeatSdkModule extends ReactContextBaseJavaModu
   }
 
   @ReactMethod
-  public void setupTracker(int withAccountId, String domain) {
+  public void setupTracker(String withAccountId, String domain) {
+    Tracker.DEBUG_MODE = true;
     Application applicationContext = (Application) reactContext.getApplicationContext();
-    Log.w("myApp", "no network");
-    Tracker.setupTracker("54876", "androidsdktest.chartbeat.com", applicationContext);
+    Tracker.setupTracker(withAccountId, domain, applicationContext);
   }
+  @ReactMethod
   public void trackView(String viewId, String viewTitle) {
     Activity currentActivity = (Activity) reactContext.getCurrentActivity();
     Tracker.trackView(currentActivity, viewId, viewTitle);
+
   }
-  public void setAuthors(Collections<String> authors) {
-    Tracker.setAuthors(authors);
+  @ReactMethod
+  public void setAuthors(ReadableArray authors) {
+    ArrayList<Object> arrayListOfAuthors = authors.toArrayList();
+
+    List<String> strings = new ArrayList<>(arrayListOfAuthors.size());
+    for (Object object : arrayListOfAuthors) {
+      strings.add(object.toString());
+    }
+    Tracker.setAuthors(strings);
   }
-  public void setSections(Collections<String> sections) {
-    Tracker.setSections(sections)
+  @ReactMethod
+  public void setSections(ReadableArray sections) {
+    ArrayList<Object> arrayListOfAuthors = sections.toArrayList();
+
+    List<String> strings = new ArrayList<>(arrayListOfAuthors.size());
+    for (Object object : arrayListOfAuthors) {
+      strings.add(object.toString());
+    }
+    Tracker.setSections(strings);
   }
 
 }
